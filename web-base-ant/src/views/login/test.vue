f2cf9c709003bd516ce8b42bc00c2463d7bd4940
zhangzixuan <2499319124@qq.com>
2024-11-14 19:42:38 +0800

    测试

---
+++ web-base-ant/src/views/login/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    ComponentName
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentName',
+  data() {
+    return {
+      // your data
+    }
+  },
+  methods: {
+    logMessage() {
+      console.log('');
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* your styles */
+</style>
