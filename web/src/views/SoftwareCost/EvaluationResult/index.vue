84d7c7de340f1ad541882b2fb0fd1ab21348c17f
ahan <2957336369@qq.com>
2024-11-19 19:46:15 +0800

    1

--- web/src/views/SoftwareCost/EvaluationResult/index.vue
+++ web/src/views/SoftwareCost/EvaluationResult/index.vue
@@ -25,6 +25,7 @@ import CostStackChart from './components/CostStackChart.vue';
 import FuctionBarChart from './components/FuctionBarChart.vue';
 import BubbleChart from './components/BubbleChart.vue';
 import { useRoute } from 'vue-router';
+import {getProjectInfo} from '@/api/EvaluationResult/index.ts';
 
 export default defineComponent({
   name: 'BigScreenView',
@@ -70,6 +71,7 @@ export default defineComponent({
             contain: 'contain',
           };
           dataContainer.fit = fitMap[queryParams.fit] || 'contain';
+
         },
         {
           immediate: true,
@@ -98,7 +100,7 @@ export default defineComponent({
                 }"
       >
         <div class="head">
-          <ViewHead title="数据可视化大屏展示"></ViewHead>
+          <ViewHead title="项目评估结果看板"></ViewHead>
         </div>
         <div class="content">
           <div class="top">
@@ -134,63 +136,73 @@ export default defineComponent({
   height: 100%;
   overflow: hidden;
   background-color: #031045c7;
+
   .big-screen-view-container {
     width: 100%;
     height: 100%;
     background-color: rgb(169, 169, 169);
     display: flex;
     flex-direction: column;
-    background-image: var(--bg-img-1);
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    background-position: center;
+    background-image: var(--bg-img-1); // 使用背景图片1作为大屏背景
+    background-repeat: no-repeat; // 背景图片不重复
+    background-size: 100% 100%; // 背景图片铺满整个容器
+    background-position: center; // 背景图片居中显示
+
     > .head {
-      height: 5.688rem;
+      height: 5.688rem; // 顶部标题区域的高度
     }
+
     > .content {
       display: flex;
-      flex-direction: column;
-      flex: 1 1 0;
+      flex-direction: column; // 内容区域按列排列
+      flex: 1 1 0; // 内容区域的高度由父容器自适应分配
       width: 100%;
       height: 0;
+
       > .top {
         width: 100%;
-        height: 12.438rem;
+        height: 12.438rem; // 上方第一行内容区域的固定高度
       }
+
       > .content {
         display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        flex: 1 1 0;
+        flex-direction: row; // 左右两列内容并排显示
+        justify-content: space-between; // 左右两列之间的空间平均分布
+        flex: 1 1 0; // 自适应分配剩余高度
         width: 100%;
         height: 0;
-        padding: 0 0.938rem 0.938rem 0.938rem;
+        padding: 0 5rem 0.938rem 5rem; // 四周内边距
         box-sizing: border-box;
+
         > .left,
         > .right {
           display: flex;
-          flex-direction: column;
+          flex-direction: column; // 每列中的内容按列排列
+
           > .box {
-            width: 100%;
-            flex: 1 1 0;
-            height: 0;
-            background-image: var(--bg-img-2);
+            width: 100%; // `box` 的宽度占满其父容器
+            flex: 1 1 0; // 高度自适应分配
+            height: 0; // 高度基于 flex 分配
+            background-image: var(--bg-img-2); // 使用背景图片2
             background-repeat: no-repeat;
             background-size: 100% 100%;
             background-position: center;
-            margin: 0 0 0.938rem 0;
+            margin: 0 0 0.938rem 0; // 每个 `box` 的下方间距
+
             &:last-child {
-              margin: 0;
+              margin: 0; // 最后一个 `box` 无下边距
             }
           }
         }
+
         > .left {
-          height: 100%;
-          width: 34.375rem;
+          height: 100%; // 左侧列的高度占满父容器
+          width: 50rem; // 左侧列的固定宽度，可修改此值改变列宽
         }
+
         > .right {
-          height: 100%;
-          width: 34.375rem;
+          height: 100%; // 右侧列的高度占满父容器
+          width: 50rem; // 右侧列的固定宽度，可修改此值改变列宽
         }
       }
     }
